5e56b47f4173d7710e317d7f13f14f31c2f21ef5
susoup <pawlica1324@gmail.com>
2023-03-30 19:27:01 +0200

    Movement script

--- Game/Assets/PlayerControl.cs
+++ Game/Assets/PlayerControl.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float speed;
+    public GameObject player;
+    private float _horizontal;
+    private float _vertical;
+    
     void Update()
     {
-        
+        Vector3 playerInput = new Vector3( _horizontal* Time.deltaTime, _vertical* Time.deltaTime,0);
+        _horizontal = Input.GetAxisRaw("Horizontal");
+        _vertical = Input.GetAxisRaw("Vertical");
+        transform.position= transform.position + playerInput.normalized *speed;
+    
     }
 }
